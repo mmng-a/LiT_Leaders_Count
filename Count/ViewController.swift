21618c741db8f77581905af571b6a7b72a0cf997
Masashi Aso <masashi.apple.japan@icloud.com>
2022-05-08 18:06:53 +0900

    done

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var label: UILabel!
+  @IBOutlet var button: UIButton!
+  
+  var count = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    button.layer.cornerRadius = 125
   }
 
-
+  @IBAction func tap() {
+    count += 1
+    label.text = count.description
+  }
 }
 
